b9576deb0c8120baff188c3ca73acc5fbdfd33a1
Revin Dennis Ramadhan <revinramadhan@gmail.com>
2023-01-14 13:19:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./src/*/*.html' / ... */"],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
